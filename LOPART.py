874d4217b6eb8a864a03e7cf8bbe063ea8934254
lamtung16 <nguyenlamtung10@gmail.com>
2023-12-15 12:26:29 -0700

    modify cumsum, modify get_mean, using plotnine

--- LOPART.py
+++ LOPART.py
@@ -1,13 +1,14 @@
 # DOCSTRING
 """
-The program applies the LOPART algorithm to analyze toy data, 
+The program applies the LOPART algorithm to analyze toy data,
 indicating a focus on testing or demonstrating the algorithm's functionality in a controlled setting.
 """
 
 
 # IMPORT
 import numpy as np
-import matplotlib.pyplot as plt
+import pandas as pd
+from plotnine import *
 
 
 # FUNCTIONS
@@ -41,48 +42,6 @@ def gen_data():
     return sequence, neg_start, neg_end, pos_start, pos_end
 
 
-# function to plot sequence with labels and changepoints wrt lambda (if provided)
-def plot_sequence(sequence, neg_start, neg_end, pos_start, pos_end, mean=None, lda=None):
-    # Create a figure and axis
-    fig, ax = plt.subplots()
-
-    # Set the figure title
-    if(lda is not None):
-        fig.suptitle('lambda = ' + str(lda))
-
-    # Plot the regions
-    bounds = (np.min(sequence) - 10, np.max(sequence) + 10)
-    for start, end in zip(neg_start, neg_end):
-        ax.fill_betweenx(bounds, start-1, end-1, color='pink', alpha=0.5, label='negative region')
-
-    for start, end in zip(pos_start, pos_end):
-        ax.fill_betweenx(bounds, start-1, end-1, color='red', alpha=0.5, label='positive region')
-
-    # Plot the sequence
-    ax.plot(sequence, marker='o', color='blue', label='sequence')
-
-    # Plot the mean
-    if(mean is not None):
-        ax.plot(mean, color='green', label='mean')
-
-    # Set axis labels
-    ax.set_xlabel('point')
-    ax.set_ylabel('value')
-
-    # Set limit
-    ax.set_xlim(-5, len(sequence) + 5)
-    ax.set_ylim(bounds)
-
-    # show legend
-    handles, labels = ax.get_legend_handles_labels()
-    unique_labels = list(set(labels))
-    unique_handles = [handles[labels.index(label)] for label in unique_labels]
-    ax.legend(unique_handles, unique_labels)
-
-    # Show the plot
-    plt.show()
-
-
 # function to create loss value from 'start' to 'end' given cumulative sum vector y (data) and z (square)
 def L(start, end, y, z):
     _y = y[end+1] - y[start]
@@ -93,18 +52,18 @@ def L(start, end, y, z):
 # function to get the list of changepoint from vector tau_star
 def trace_back(tau_star):
     tau = tau_star[-1]
-    chpnt = np.array([len(tau_star)], dtype=int)
+    chpnt = np.array([len(tau_star)-1], dtype=int)
     while tau > 0:
         chpnt = np.append(tau, chpnt)
         tau = tau_star[tau]
-    return np.append(0, chpnt)
+    return np.append(-1, chpnt)
 
 
 # function to get the mean vector of the sequence from the list of changepoint
-def get_mean(sequence, chpnt):
-    mean = np.zeros(len(sequence))
+def get_mean(y, chpnt):
+    mean = np.zeros(len(y)-1)
     for i in range(len(chpnt)-1):
-        mean[chpnt[i]:chpnt[i+1]] = np.mean(sequence[chpnt[i]:chpnt[i+1]])
+        mean[chpnt[i]+1:chpnt[i+1]+1] = (y[chpnt[i+1]+1] - y[chpnt[i]+1])/(chpnt[i+1] - chpnt[i])
     return mean
 
 
@@ -127,7 +86,7 @@ def get_T(sequence_length, neg_start, neg_end, pos_start, pos_end):
 
         if (len(T) == i):                                                                           # otherwise
             T.append(T[i-1]+[i-1])
-    
+
     return T
 
 
@@ -162,32 +121,83 @@ def lopart(lda, T, sequence, y, z):
 
     # get set of changepoints
     set_of_chpnt = trace_back(tau_star)
-    
+
     return set_of_chpnt
 
 
-# Main block
+# function to plot sequence with labels and changepoints wrt lambda (if provided)
+def plot_sequence(sequence, neg_start, neg_end, pos_start, pos_end, chpnt=None, mean=None, lda=None):
+    # Prepare data for plotnine
+    data = pd.DataFrame({'point': range(len(sequence)), 'value': sequence})
+
+    # Create the plot
+    plot = (
+        ggplot(data, aes(x='point', y='value')) +
+        theme_minimal() +
+        labs(x='point', y='value') +
+        xlim(-5, len(sequence) + 5) +
+        ylim(np.min(sequence) - 10, np.max(sequence) + 10)
+    )
+
+    # Add negative regions
+    for start, end in zip(neg_start, neg_end):
+        plot += geom_rect(aes(xmin=start-1, xmax=end-1, ymin=np.min(sequence) - 10, ymax=np.max(sequence) + 10),
+                          fill='pink', alpha=0.2, color='black', size=0)
+
+    # Add positive regions
+    for start, end in zip(pos_start, pos_end):
+        plot += geom_rect(aes(xmin=start-1, xmax=end-1, ymin=np.min(sequence) - 10, ymax=np.max(sequence) + 10),
+                          fill='red', alpha=0.2, color='black', size=0)
+    
+    # Add data points
+    plot += geom_point(color='blue', size=2, alpha=0.7)
+
+    # Add mean line
+    if mean is not None:
+        for i in range(len(chpnt) - 1):
+            plot += geom_segment(aes(x=chpnt[i], y=mean[chpnt[i]+1], xend=chpnt[i+1], yend=mean[chpnt[i]+1]),
+                            color='green', size=1.5, alpha=0.7)
+    # Add changepoint
+    if chpnt is not None:
+        for i in range(1, len(chpnt)-1):
+            plot += geom_vline(xintercept=chpnt[i], linetype='dashed', color='yellow', alpha=0.8)
+
+    # Set the figure title
+    if lda is not None:
+        plot += ggtitle('lambda = ' + str(lda))
+
+    # Center the title horizontally
+        plot += theme(plot_title=element_text(hjust=0.5))
+    
+    # Add legend
+    plot += labs(color="Legend")  # Legend title
+    plot += guides(color=guide_legend(title="Legend"))  # Legend label
+
+    # return the plot
+    return plot
+
+
+# MAIN BLOCK
 if __name__ == "__main__":
     # generate data
     sequence, neg_start, neg_end, pos_start, pos_end = gen_data()
     sequence_length = len(sequence)
-    
-    # plot data
-    plot_sequence(sequence, neg_start, neg_end, pos_start, pos_end)
 
     # vectors of cumulative sums
-    y = np.zeros(sequence_length + 1)       # vectors of cumulative sums of data
-    z = np.zeros(sequence_length + 1)       # vectors of cumulative sums of squares
+    y = np.cumsum(sequence)
+    z = np.cumsum(np.square(sequence))
 
-    for i in range(1, sequence_length+1):
-        y[i] = y[i-1] + sequence[i-1]
-        z[i] = z[i-1] + np.square(sequence[i-1])
-    
+    y = np.append([0], y)
+    z = np.append([0], z)
+
     # get T
     T = get_T(sequence_length, neg_start, neg_end, pos_start, pos_end)
 
-    # plot solution with respect to different lambda
+    # save images of solution with respect to different lambda into one pdf file
+    plots = [plot_sequence(sequence, neg_start, neg_end, pos_start, pos_end)]
     for lda in [0, 1, 10, 100, 1000, 10000, 100000]:
         chpnt = lopart(lda, T, sequence, y, z)
-        mean  = get_mean(sequence, chpnt)
-        plot_sequence(sequence, neg_start, neg_end, pos_start, pos_end, mean, lda)
+        mean  = get_mean(y, chpnt)
+        plots.append(plot_sequence(sequence, neg_start, neg_end, pos_start, pos_end, chpnt, mean, lda))
+    
+    save_as_pdf_pages(plots, filename='output.pdf')
