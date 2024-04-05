d4e61f2c91f3d0402e821f45f8aecaa9ed2005f0
lamtung16 <nguyenlamtung10@gmail.com>
2024-04-05 16:08:37 -0700

    update code

---
+++ 0.lopart_code/README.md
@@ -0,0 +1,48 @@
+# Changepoint detection
+## Problem
+- What we have:
+    - The number of sequence: 413
+    - The number of data points per sequence: ranges from 39 to 43628 
+    - The number of labels (regions) per sequence: ranges from 2 to 12
+        - Negative regions $[a, b]$: neither of $a, a+1, \dots, b-1$ can be a changepoint.
+        - Positive regions $[a, b]$: either $a, a+1, \dots, b-1$ can be a changepoint.
+- What we have to find:
+    - For each sequence $x = [x_1, x_2, \dots, x_N]$, find the set of changepoint $f \subset \{1, 2, \dots, N-1\}$ to minimize the number of incorect predicted labels.
+
+## Algorithm
+### Optimal Partitioning (OPART)
+To implement the algorithm, we need to know the definition of this loss function:
+$$L(x, t_1, t_2) = \min_{\mu} \sum_{i=t_1}^{t_2} (x_i - \mu)^2 = \sum_{i=t_1}^{t_2}(x_i - \frac{\sum_{j=t_1}^{t_2}x_i}{t_2-t_1+1})^2=\sum_{i=t_1}^{t_2}x_i^2-\frac{(\sum_{i=t_1}^{t_2}x_t)^2}{t_2-t_1+1}$$
+Pseudo code (last changepoint algorithm):
+- input:
+    - sequence: $x = [x_1, x_2, \dots, x_N]$
+    - penalty constant: $\lambda \geq 0$
+- algorithm:
+    - $C_0 = -\lambda$
+    - for $t:1 \rightarrow N$:
+        - $C_t = \min_{\tau \in \{0, 1, \dots, t-1\}} C_\tau + \lambda + L(x, \tau + 1, t)$
+        - $\tau^*_t = \argmin_{\tau \in \{0, 1, \dots, t-1\}} C_\tau + \lambda + L(x, \tau + 1, t)$
+    - getting last change point vector $\tau^* = [0, \tau^*_2, \tau^*_3, \dots, \tau^*_N]$
+- output:
+    - from vector $\tau^*$, traceback from $\tau^*_N$ to get the set of changepoint
+
+### Labled Optimal Partitioning (LOPART)
+Similar to OPART, there is only one difference: inside the for loop, instead of consider $\tau \in \{0, 1, \dots, t-1\}$, we consider $\tau \in T$ which satisfy the number of changepoints from all of the train labels.
+
+## Penalty Value Learning
+To choose the best value of $\lambda$ (apply for either OPART or LOPART), we use the train set to learn the best $\lambda$ then apply that $\lambda$ to the test set. The train set and the test set have the same 413 sequences but different labels for each sequence.
+
+### Bayesian Information Criterion (BIC)
+Set $\lambda = \log(N)$ where $N$ is the length of the sequence. For example, $N = 100$, then $\lambda \approx 4.6$.
+
+### Constant
+Try $\lambda = 10^k$ where $k \in \{-5, -4.5, \dots, 5\}$.
+Then choose a value with minimal train label errors, then
+predicts this constant $\lambda$ for each test data sequence.
+
+### Linear
+- From the train set, for each sequence, use OPART with 21 different $\lambda$ from the previous part, and for each $\lambda$, we get the total number of train error.
+- Then each sequence $i$, we have $\lambda = 10^{l_i} \rightarrow 10^{h_i}$ which minimize the train error labels.
+- consider $\log(\lambda_i) = \log(\log(N_i))*w + b = x_iw + b$
+- Because we want $\log(\lambda_i)$ is between $l_i$ and $h_i$, so we use the hinge square loss (margin = 1) which is similar to mean square error (but instead of having one minimal point, hinge square function has a range of minimal points) for learning $w$ and $b$:
+$$L(\lambda_i, l_i, h_i) = \big(ReLU(x_iw+b - h_i + 1) + ReLU(l_i - x_iw - b + 1)\big)^2$$
--- utility_functions.py
+++ utility_functions.py
@@ -1,99 +1,67 @@
+
 import numpy as np
 import pandas as pd
-import gzip
 import csv
 import os
 import torch
 import torch.nn as nn
 
 
-# Get cumulative sum vectors
-def get_cumsum(sequence):
-    y = np.cumsum(sequence)
-    z = np.cumsum(np.square(sequence))
-    return np.append([0], y), np.append([0], z)
+
+# get number of error from sequenceID and llda
+def get_err(evaluation_df, seqID, llda):
+    # get sub eval_df of seqID
+    eval_df = evaluation_df[evaluation_df['sequenceID'] == seqID]
+    
+    # get right row
+    position = np.logical_and(eval_df['min.log.lambda'] < llda, llda < eval_df['max.log.lambda'])
+    row = eval_df[position]
+
+    # get total labels and total errors
+    n_labels = row['labels'].item()
+    n_errs = row['errors'].item()
+
+    return n_labels, n_errs
 
 
-# function to create loss value from 'start' to 'end' given cumulative sum vector y (data) and z (square)
-def L(start, end, y, z):
-    _y = y[end+1] - y[start]
-    _z = z[end+1] - z[start]
-    return _z - np.square(_y)/(end-start+1)
+
+# add row to csv
+def add_row_to_csv(path, head, row):
+    file_exists = os.path.exists(path)              # Check if the file exists
+    is_row_exist = False                            # default False for is_row_exist
+    with open(path, 'a', newline='') as csvfile:    # Open the CSV file in append mode
+        writer = csv.writer(csvfile)
+        if not file_exists:                         # If the file doesn't exist, write the header
+            writer.writerow(head)
+        writer.writerow(row)                        # Write the row
 
 
-# function to get the list of changepoint from vector tau_star
-def trace_back(tau_star):
-    tau = tau_star[-1]
-    chpnt = np.array([len(tau_star)], dtype=int)
-    while tau > 0:
-        chpnt = np.append(tau, chpnt)
-        tau = tau_star[tau-1]
-    return np.append(0, chpnt)
 
 
-# counting errors
-def error_count(chpnt, neg_start, neg_end, pos_start, pos_end):
-    chpnt = chpnt[1:]
-    fp = 0
-    fn = 0
-    for ns, ne in zip(neg_start, neg_end):
-        count = sum(1 for cp in chpnt if ns <= cp < ne)
-        if count >= 1:
-            fp += 1
-    for ps, pe in zip(pos_start, pos_end):
-        count = sum(1 for cp in chpnt if ps <= cp < pe)
-        if count >= 2:
-            fp += 1
-        elif count == 0:
-            fn += 1
-    return fp, fn
+# get acc from eval_df and lldas_df
+def get_acc(eval_df, lldas_df):
+    total_err = 0
+    total_labels = 0
+    for seqID in lldas_df['sequenceID']:
+        llda = lldas_df[lldas_df['sequenceID'] == seqID]['llda'].item()
+        n_labels, n_errs = get_err(eval_df, seqID, llda)
+        total_labels += n_labels
+        total_err += n_errs
+    acc = (total_labels - total_err)/total_labels
+    return acc*100
 
 
-# OPART given lambda and sequence
-def opart(lda, sequence):
-    y, z = get_cumsum(sequence)             # cumsum vector
-    sequence_length = len(sequence)-1       # length of sequence
-
-    # Set up
-    C = np.zeros(sequence_length + 1)
-    C[0] = -lda
-
-    # Get tau_star
-    tau_star = np.zeros(sequence_length+1, dtype=int)
-    for t in range(1, sequence_length+1):
-        V = C[:t] + lda + L(1 + np.arange(t), t, y, z)  # calculate set V
-        last_chpnt = np.argmin(V)                       # get optimal tau from set V
-        C[t] = V[last_chpnt]                            # update C_i
-        tau_star[t] = last_chpnt                        # update tau_star
-
-    set_of_chpnt = trace_back(tau_star[1:])             # get set of changepoints
-    return set_of_chpnt
 
 
-# Loss function
+# Hinged Square Loss
 class SquaredHingeLoss(nn.Module):
     def __init__(self, margin=1):
         super(SquaredHingeLoss, self).__init__()
         self.margin = margin
 
     def forward(self, predicted, y):
-        low, high = y[:, 0], y[:, 1]
-        margin = self.margin
-        loss_low = torch.relu(low - predicted + margin)
-        loss_high = torch.relu(predicted - high + margin)
+        low, high = y[:, 0:1], y[:, 1:2]
+        loss_low = torch.relu(low - predicted + self.margin)
+        loss_high = torch.relu(predicted - high + self.margin)
         loss = loss_low + loss_high
         return torch.mean(torch.square(loss))
-
-
-# Show error
-def show_error_rate(df):
-    fold1_total_errs = df['fold_1_fp_errs'].sum() + df['fold_1_fn_errs'].sum()
-    fold2_total_errs = df['fold_2_fp_errs'].sum() + df['fold_2_fn_errs'].sum()
-
-    fold1_total_labels = df['fold_1_total_labels'].sum()
-    fold2_total_labels = df['fold_2_total_labels'].sum()
-
-    rate1 = 100*(fold1_total_labels - fold1_total_errs)/fold1_total_labels
-    rate2 = 100*(fold2_total_labels - fold2_total_errs)/fold2_total_labels
-
-    return rate1, rate2, fold1_total_labels, fold2_total_labels, fold1_total_errs, fold2_total_errs
